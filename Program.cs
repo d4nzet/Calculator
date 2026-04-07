75eef9c1aa907b996852f3991f8f2576d1d13af2
d4nzet <humania57@gmail.com>
2026-04-07 14:58:40 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
-﻿int a = 10, b = 6;
+﻿// See https://aka.ms/new-console-template for more information
+int var1 = 100;
+int var2 = 67;
 
-static int Penjumlahan(int a, int b)
+static int Penjumlahan(int var1, int var2)
 {
-    return a + b;
+    return var1 + var2;
 }
 
-static int Pengurangan(int a, int b)
+static int Pengurangan(int var1, int var2)
 {
-    return a - b;
-
+    return var1 - var2;
 }
+
+static int Perkalian(int var1, int var2)
+{
+    return var1 * var2;
+}
+
+static int Pembagian(int var1, int var2)
+{
+    return var1 / var2;
+}
+
+Console.WriteLine("Penjumlahan: " + var1 + " + " + var2 + " = " + Penjumlahan(var1, var2));
+Console.WriteLine("Pengurangan: " + var1 + " - " + var2 + " = " + Pengurangan(var1, var2));
+Console.WriteLine("Perkalian: " + var1 + " * " + var2 + " = " + Perkalian(var1, var2));
+Console.WriteLine("Pembagian: " + var1 + " / " + var2 + " = " + Pembagian(var1, var2));
